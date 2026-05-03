6709a9a3f5af1143a6ecb02196dea57d5df7cce8
Karthik <karth@mini.local>
2026-05-03 14:53:14 +0530

    Configure Django to serve React frontend directly from dist for seamless local deployment

--- mini/urls.py
+++ mini/urls.py
@@ -15,11 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
+from django.views.static import serve
+import os
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('myapp.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + [
+    re_path(r'^assets/(?P<path>.*)$', serve, {'document_root': os.path.join(settings.BASE_DIR, 'frontend', 'dist', 'assets')}),
+    re_path(r'^vite\.svg$', serve, {'document_root': os.path.join(settings.BASE_DIR, 'frontend', 'dist'), 'path': 'vite.svg'}),
+    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
+]
